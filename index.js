42498cd0cbb45bdf32855e139e0503232a80071b
saju-79 <saju98622@gmail.com>
2026-03-01 00:13:53 +0600

    url add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 const app = express()
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://plantnet-31532.web.app', 'https://your-site.netlify.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
